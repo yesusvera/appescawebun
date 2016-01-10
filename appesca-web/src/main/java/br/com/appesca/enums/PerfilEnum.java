437ee24fb054b058e566cb26e696b64752b0e349
yesus <yesusvera@gmail.com>
2016-01-10 21:16:53 -0200

    Permissões de usuário.

--- appesca-web/src/main/java/br/com/appesca/enums/PerfilEnum.java
+++ appesca-web/src/main/java/br/com/appesca/enums/PerfilEnum.java
@@ -2,24 +2,34 @@ package br.com.appesca.enums;
 
 public enum PerfilEnum {
 
-	ADMINISTRADOR(0, "Administrador"), 
-	COORDENADOR(1, "Coordenador"), 
-	DEGRAVADOR(2, "Degravador"), 
-	PESQUISADOR(3, "Pesquisador");
+	ADMINISTRADOR(0, "Administrador", "fa-user-secret"), 
+	COORDENADOR(1, "Coordenador", "fa-users"), 
+	DEGRAVADOR(2, "Degravador", "fa-file-audio-o"), 
+	PESQUISADOR(3, "Pesquisador", "fa-street-view");
 	
 	private final int valor;
 	private final String desc;
+	private final String fontAweSomeIcon;
 	
-	private PerfilEnum(int valor, String desc) {
+	private PerfilEnum(int valor, String desc, String fontAweSomeIcon) {
 		this.valor = valor;
 		this.desc = desc;
+		this.fontAweSomeIcon = fontAweSomeIcon;
 	}
 	
 	public int getValor(){
 		return valor;
 	}
-	
+
 	public String getDescricao(){
 		return desc;
+	}
+	
+	public String getLabelSelect(){
+		return "<i class='fa #{usr.perfil.fontAweSomeIcon} fa-2x'/>"+desc;
+	}
+	
+	public String getFontAweSomeIcon(){
+		return fontAweSomeIcon;
 	}
 }
